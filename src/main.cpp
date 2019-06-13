79f18fa8bc1a99f24e2ea9c39fe037f0995c9ccd
Bert Belder <bertbelder@gmail.com>
2019-06-13 11:44:26 +0200

    Begin implementation

--- src/main.cpp
+++ src/main.cpp
@@ -11,3 +11,49 @@
 #include "clang/Rewrite/Core/Rewriter.h"
 #include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
+
+#include <iostream>
+
+using namespace std;
+using namespace clang;
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace clang::ast_matchers;
+using namespace llvm;
+
+class MatchActionImpl : public MatchFinder::MatchCallback {
+public:
+  void run(const MatchFinder::MatchResult& result) override {
+    auto node = result.Nodes.getNodeAs<NamedDecl>("decl");
+    std::cout << node->getNameAsString() << std::endl;
+  }
+};
+
+class ASTConsumerImpl : public ASTConsumer {
+  void HandleTranslationUnit(ASTContext& ast) override {
+    // Run the matchers when we have the whole TU parsed.
+    MatchActionImpl callback;
+    MatchFinder finder;
+    finder.addMatcher(
+        namedDecl(hasAncestor(namespaceDecl(hasName("::v8")))).bind("decl"),
+        &callback);
+    finder.matchAST(ast);
+  }
+};
+
+class FrontendActionImpl : public ASTFrontendAction {
+public:
+  std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance& CI,
+                                                 StringRef file) override {
+    return llvm::make_unique<ASTConsumerImpl>();
+  }
+};
+
+int main(int argc, const char** argv) {
+  cl::OptionCategory opt_category("gen8");
+  tooling::CommonOptionsParser opt_parser(argc, argv, opt_category);
+
+  tooling::ClangTool tool(opt_parser.getCompilations(),
+                          opt_parser.getSourcePathList());
+  return tool.run(newFrontendActionFactory<FrontendActionImpl>().get());
+}
